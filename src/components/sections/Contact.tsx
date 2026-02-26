1ead15101fb720481c0cee39b03cb55a6a2fa6ea
Guillaume Morvan <g.morvan35pro@gmail.com>
2026-02-26 10:49:25 +0100

    modification contact CV

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -225,7 +225,7 @@ const handleSubmit = (e: React.FormEvent) => {
                         <h4 className="text-lg font-medium text-white">Mon CV</h4>
                   <a
                     href="/public/docs/fernandes-sebastien-cv.pdf"
-                    download
+                    target = "_blank"
                     className="w-full mt-6 px-6 py-3 bg-gradient-to-r from-orange-500 to-purple-500 text-white rounded-xl flex items-center justify-center gap-2 hover:opacity-90 transition-opacity"
                   >
                     
