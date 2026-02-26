d978f18c2e3fc6342a65574b05b03ccd8944e32f
Guillaume Morvan <g.morvan35pro@gmail.com>
2026-02-26 23:39:41 +0100

    modification générale

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -91,7 +91,7 @@ const About: React.FC = () => {
                     <div className="absolute inset-0 bg-gradient-to-br from-orange-500 to-purple-500 rounded-full animate-spin-slow"></div>
                     <div className="absolute inset-1 bg-gray-800 rounded-full overflow-hidden">
                       <img
-                        src="/images/profile.webp"
+                        src="/images/profil.jpg"
                         alt="Profile"
                         className="w-full h-full object-cover"
                       />
